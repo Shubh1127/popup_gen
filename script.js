74716a2a27ca41f421002e55616cebb2d183ff54
shubham <shubhamsinghmor2312@gmail.com>
2024-08-01 22:13:42 +0530

    first changes

---
+++ script.js
@@ -0,0 +1,9 @@
+let popup=document.getElementById("popup");
+// let button=document.getElementById("close");
+
+function openPopup(){
+    popup.classList.add("open-popup")
+}
+function closePopup(){
+    popup.classList.remove("open-popup")
+}
